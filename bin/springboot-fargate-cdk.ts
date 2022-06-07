f7dd4e438553e88735dc547e25b15a2a73dc9b4f
alaa2sbateen <@Alaa.12345>
2022-06-07 13:18:12 +0300

    infra

--- bin/springboot-fargate-cdk.ts
+++ bin/springboot-fargate-cdk.ts
@@ -5,4 +5,3 @@ import { SpringbootFargateCdkStack } from '../lib/springboot-fargate-cdk-stack';
 
 const app = new cdk.App();
 const springbootApp = new SpringbootFargateCdkStack(app, 'SpringbootFargateCdkStack');
-cdk.Tags.of(springbootApp).add('Project', 'Java-PRJ1');
